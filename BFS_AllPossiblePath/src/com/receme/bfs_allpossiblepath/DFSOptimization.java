2f5811794498673e20a6b5e60b8c4c37081214f4
tian <976969426@qq.com>
2016-03-16 12:03:08 +0800

    55

---
+++ BFS_AllPossiblePath/src/com/receme/bfs_allpossiblepath/DFSOptimization.java
@@ -0,0 +1,108 @@
+package com.receme.bfs_allpossiblepath;
+
+import java.util.Map;
+import java.util.Stack;
+import java.util.Vector;
+
+public class DFSOptimization {
+    private Stack<Node> nodeStack;
+    private Node startNode;
+    private Node endNode;
+    private int minPathLength;
+    private Vector<Node> min_path;
+    private Map<Integer, Vector<Node>> graph;
+    public int sum = 0;
+
+    public DFSOptimization(Map<Integer, Vector<Node>> graph, Node _startNode,
+                           Node _endNode) {
+        this.graph = graph;
+        minPathLength = -1;
+        nodeStack = new Stack<>();
+        min_path = new Vector<Node>();
+        this.startNode = _startNode;
+        this.endNode = _endNode;
+    }
+
+    public void getAvailablePath() {
+        DFSImplementation();
+    }
+
+    private Node getUnvisitedChildNode(Node parent) {
+        Node result = null;
+        Vector<Node> vector = graph.get(parent.val);
+        int i = 0;
+        for (Node node1 : vector) {
+            if ((vistied[node1.val] != 1) && (!node1.isVisited)) {
+                result = node1;
+                result.isVisited = true;
+                vistied[node1.val] = 1;
+                break;
+            }
+            if (i >= 2) {
+                break;
+            }
+            i++;
+        }
+        return result;
+    }
+
+    private void setChildUnVistited(Node parent) {
+        Vector<Node> vector = graph.get(parent.val);
+        for (Node node1 : vector) {
+            node1.isVisited = false;
+        }
+    }
+
+    int[] vistied = new int[600];
+
+    private void DFSImplementation() {
+        vistied[startNode.val] = 1;
+        nodeStack.add(startNode);
+        while (!nodeStack.isEmpty()) {
+            Node node = nodeStack.peek();
+//            System.out.println(node.val);
+            Node childNode = getUnvisitedChildNode(node);
+            if (childNode != null) {
+                nodeStack.add(childNode);
+                if (childNode.val == endNode.val) {
+                    sum++;
+                    printLength(nodeStack);
+                    vistied[nodeStack.peek().val] = 0;
+                    setChildUnVistited(nodeStack.peek());
+                    nodeStack.pop();
+                }
+            } else {
+                vistied[nodeStack.peek().val] = 0;
+                setChildUnVistited(nodeStack.peek());
+                nodeStack.pop();
+            }
+        }
+    }
+
+    private void printLength(Stack<Node> path) {
+        for (Node node : path) {
+            System.out.print(node.val + "   ");
+        }
+        System.out.println();
+    }
+
+    private int getPathLength(Stack<Node> path) {
+        int sum = 0;
+        for (Node node : path) {
+            sum += node.val;
+        }
+        return sum;
+    }
+
+    public void sortGraphNode(Map<Integer, Vector<Node>> graph) {
+        for (Map.Entry<Integer, Vector<Node>> map : graph.entrySet()) {
+            sortNode(map.getValue());
+        }
+    }
+
+    public void sortNode(Vector<Node> nodes) {
+        for(Node node : nodes){
+
+        }
+    }
+}
--- BFS_AllPossiblePath/src/com/receme/bfs_allpossiblepath/Main.java
+++ BFS_AllPossiblePath/src/com/receme/bfs_allpossiblepath/Main.java
@@ -1,48 +1,67 @@
 package com.receme.bfs_allpossiblepath;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-import java.util.Vector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	//1466
+    //1466
 //	169410
-	public static Map<Integer, Vector<Node> > graph = null;
-	public Vector<Node> nodes = null;
-	public static HashMap v;
-	static Vector<Integer> as;
-	int a = 5;
+    public static Map<Integer, Vector<Node>> graph = null;
+    public Vector<Node> nodes = null;
+    public static HashMap v;
+    static Vector<Integer> as;
+    int a = 5;
+    public static List<Integer> list = null;
 
-	public static void main(String[] args){
-		
-		/*graph = {'A': ['B', 'C','E'],
-	             'B': ['A','C', 'D'],
-	             'C': ['D'],
-	             'D': ['C'],
-	             'E': ['F','D'],
-	             'F': ['C']}
-		*/
-//		Stack<Main> stack = new Stack<>();
-//		stack.add(new Main());
-//		Main main = stack.peek();
-//		main.a = 10;
-//		System.out.println(stack.peek().a);
-		InputGraph in = new InputGraph();
-		graph = new HashMap<Integer, Vector<Node>>();
-		graph = in.getInput();
+    public static void main(String[] args) throws IOException {
+        list = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/demand.csv")));
+        String s = bufferedReader.readLine();
+        String[] splits = s.split(",");
+        System.out.println(splits[2]);
+//		String[] mustNodes = splits[2].split("|");
+//		for(String mustNode: mustNodes){
+//			System.out.println(mustNode);
+//			list.add(Integer.valueOf(mustNode));
+//		}
+        bufferedReader.close();
+        graph = new HashMap<Integer, Vector<Node>>();
+        bufferedReader = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/topo.csv")));
+        while ((s = bufferedReader.readLine()) != null) {
+//            System.out.println(s);
+            splits = s.split(",");
+//			for(int i = 0; i < splits.length; i++){
+//				System.out.print(splits[i]+"  ");
+//			}
+//            System.out.println();
+            if (graph.containsKey(Integer.valueOf(splits[1]))) {
+                Vector<Node> nodes = graph.get(Integer.valueOf(splits[1]));
+                nodes.add(new Node(Integer.valueOf(splits[2]), Integer.valueOf(splits[3])));
+                graph.put(Integer.valueOf(splits[1]), nodes);
+            } else {
+                Vector<Node> nodes = new Vector<>();
+                nodes.add(new Node(Integer.valueOf(splits[2]), Integer.valueOf(splits[3])));
+                graph.put(Integer.valueOf(splits[1]), nodes);
+            }
+        }
+        for (Map.Entry<Integer, Vector<Node>> map : graph.entrySet()) {
+            System.out.print(map.getKey() + "  ");
+            for (Node node : map.getValue()) {
+                System.out.print(node.val + "  ");
+            }
+            System.out.println();
+        }
 		long time = System.currentTimeMillis();
-		DFS dfs = new DFS(graph, new Node(1), new Node(10));
-		dfs.getAvailablePath();
-		System.out.println(System.currentTimeMillis() - time);
-		System.out.println(dfs.sum);
-//		BFS bfsAlgo = new BFS(graph, new Node(1), new Node(10));
-//		bfsAlgo.getAvailablePath();
-//		bfsAlgo.getMinPath();
+//		BFS dfs = new BFS(graph, new Node(2), new Node(19));
+//		dfs.getAvailablePath();
 //		System.out.println(System.currentTimeMillis() - time);
-//		System.out.println(bfsAlgo.sum);
-	}
-
-
-	
-
+//		System.out.println(dfs.sum);
+		BFS bfsAlgo = new BFS(graph, new Node(2), new Node(19));
+		bfsAlgo.getAvailablePath();
+		bfsAlgo.getMinPath();
+		System.out.println(System.currentTimeMillis() - time);
+		System.out.println(bfsAlgo.sum);
+    }
 }
